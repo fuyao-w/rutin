1ee1511eb0a4d2ab31708c935bab6d45a80f8dde
wangfuyao <815775754@qq.com>
2022-08-08 21:46:42 +0800

    feat:单测

--- iokit/codec_test.go
+++ iokit/codec_test.go
@@ -1,35 +1,37 @@
 package iokit
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 )
 
-func TestName(t *testing.T) {
-	var bytes []byte
-	d := DelimParser{
-		Delim: '-',
-	}
-	//abytes, err := d.Encode([]byte("aaa/"))
-	//t.Log(string(abytes), err)
-	abytes := []byte("aaa//\\-/-")
-	for _, n := range abytes {
+type read []byte
 
-		if n == d.Delim {
-			_ = "sfdsf\r\nsdfsdf"
-			if len(bytes) > 0 && bytes[len(bytes)-1] == '/' {
-				bytes = append(bytes, n)
-			} else {
-				delim := string([]byte{d.Delim})
-				result := strings.ReplaceAll(string(bytes), fmt.Sprintf("/%s", delim), delim)
-				bytes = []byte(result)
-				break
-			}
-		} else {
-			bytes = append(bytes, n)
-		}
-	}
-	t.Log(string(bytes))
+func TestName(t *testing.T) {
+
+	//var bytes []byte
+	d := DelimParser{}
+
+	res, err := d.EncodeC([]byte("%$$%$$$"))
+	t.Log(string(res), err)
+	////abytes, err := d.Encode([]byte("aaa/"))
+	////t.Log(string(abytes), err)
+	//abytes := []byte("aaa//\\-/-")
+	//for _, n := range abytes {
+	//
+	//	if n == d.Delim {
+	//		_ = "sfdsf\r\nsdfsdf"
+	//		if len(bytes) > 0 && bytes[len(bytes)-1] == '/' {
+	//			bytes = append(bytes, n)
+	//		} else {
+	//			delim := string([]byte{d.Delim})
+	//			result := strings.ReplaceAll(string(bytes), fmt.Sprintf("/%s", delim), delim)
+	//			bytes = []byte(result)
+	//			break
+	//		}
+	//	} else {
+	//		bytes = append(bytes, n)
+	//	}
+	//}
+	//t.Log(string(bytes))
 
 }
